d9002e736fa2f155034dfe11a2b951655f430d9a
C. E. Sharp <sharpc@uk.ibm.com>
2017-11-10 14:43:37 +0000

    Added helm initialization for public cloud

--- login_public.sh
+++ login_public.sh
@@ -6,3 +6,6 @@ bx login -a $API_ENDPOINT -u $BX_USERNAME -p $BX_PASSWORD -c $BX_ACCOUNT
 bx cr login
 bx cs init
 eval `bx cs cluster-config $BX_CLUSTER --export`
+
+#Setup Helm
+helm init --client-only
