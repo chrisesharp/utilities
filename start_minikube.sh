5a617d8b1ec19717baf9cf4128630f056373aac5
C. E. Sharp <sharpc@uk.ibm.com>
2017-11-20 14:45:38 +0000

    adding helm init to startup

--- start_minikube.sh
+++ start_minikube.sh
@@ -7,6 +7,7 @@ sleep 3
 
 echo "Starting an insecure docker registry within minikube..."
 kubectl create -f kubernetes/minikube-registry.yaml 
+helm init 
 
 echo "You will need to add 192.168.99.100:32767 as an insecure registry to your docker daemon"
 popd
